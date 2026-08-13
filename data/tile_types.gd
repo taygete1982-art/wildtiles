03920d68910dfe165da65d84225c622c69321cd2
taygete1982-art <taygete1982@gmail.com>
2026-08-14 00:43:37 +0300

    feat: add GameManager and tile types

---
+++ data/tile_types.gd
@@ -0,0 +1,15 @@
+﻿# Типы плиток в игре
+const TILE_TYPES = [
+    "giraffe",
+    "lion",
+    "zebra",
+    "meerkat",
+    "acacia",
+    "sloth",
+    "tiger",
+    "chameleon",
+    "monstera",
+    "camel",
+    "fennec",
+    "cactus"
+]
---
+++ scripts/GameManager.gd
@@ -0,0 +1,12 @@
+﻿extends Node
+
+var score: int = 0
+var current_level: int = 1
+
+func start_game():
+    score = 0
+    print("Игра началась!")
+
+func add_score(points: int):
+    score += points
+    print("Счёт: ", score)
